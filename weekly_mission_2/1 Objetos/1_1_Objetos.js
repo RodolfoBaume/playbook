85fde3e410f62b87fbe8fc6a671e05cadc24ecf6
RodolfoBaume <robalaz@hotmail.com>
2022-04-13 21:23:01 -0500

    Añadir 1_1_Objetos.js

---
+++ weekly_mission_2/1 Objetos/1_1_Objetos.js
@@ -0,0 +1,7 @@
+console.log("Objetos")
+
+// Ejemplo 1: Crear un objeto
+const myObject = {} // Esto es un objeto vacío
+console.log("Ejemplo 1: Crear un objeto pelón o vacío")
+console.log(myObject)
+
