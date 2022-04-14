f05f43dd87ab813c3d7e9b680ae93ab554702238
RodolfoBaume <robalaz@hotmail.com>
2022-04-13 21:27:50 -0500

    Añadir 1_3_Objetos.js

---
+++ weekly_mission_2/1 Objetos/1_3_Objetos.js
@@ -0,0 +1,21 @@
+console.log("Objetos")
+
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+  name: "Reboton",
+  age: 6,
+  nicknames: [
+   "Rebotas",
+    "Rebotitas",
+    "Peludo"
+  ],
+  address: {
+    zip_code: "42020",
+    street: "Calle 4, Real de Medinas",
+    city: "Pachuca"
+  }
+}
+console.log("Ejemplo 3: Objeto con diferentes propiedades")
+console.log(myObject3)
+console.log(myObject3.name)
+console.log(myObject3["address"])
