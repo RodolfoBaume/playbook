2fff7828962ee827f3cda72910a665cdaeb93ce5
RodolfoBaume <robalaz@hotmail.com>
2022-04-13 21:27:03 -0500

    Añadir 1_2_Objetos.js

---
+++ weekly_mission_2/1 Objetos/1_2_Objetos.js
@@ -0,0 +1,10 @@
+console.log("Objetos")
+
+// Ejemplo 2: Crear un objeto con propiedades
+const myObject2 = {
+  name: "Rodolfo",
+  age: 46
+}
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObject2)
+
