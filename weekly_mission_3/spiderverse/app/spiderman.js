fec6ca207996f1df9c5ad74669ae3a290c755f8e
RodolfoBaume <robalaz@hotmail.com>
2022-04-20 13:19:20 -0500

    Se agrego el constructor de la clase Spiderman

--- weekly_mission_3/spiderverse/app/spiderman.js
+++ weekly_mission_3/spiderverse/app/spiderman.js
@@ -1,4 +1,8 @@
-class Spiderman {}
+class Spiderman {
+    constructor(name){
+        this.name = name
+    }
+}
   
   // Esta línea nos permite exportar nuestra clase
   module.exports = Spiderman
