56bdfdb867ed0777a8edb8f8765d137db1b58c23
RodolfoBaume <robalaz@hotmail.com>
2022-04-20 13:47:15 -0500

    Se agrego metodo getInfo a clase Spiderman

--- weekly_mission_3/spiderverse/app/spiderman.js
+++ weekly_mission_3/spiderverse/app/spiderman.js
@@ -1,10 +1,13 @@
 class Spiderman {
     constructor(name, age, actor, movies, studio){
-        this.name = name,
-        this.age = age,
-        this.actor = actor,
-        this.movies = movies,
+        this.name = name
+        this.age = age
+        this.actor = actor
+        this.movies = movies
         this.studio = studio
+    }
+    getInfo(){
+        return `Hey, I'm ${this.actor} from ${this.studio} studio`
     }
 }
   
